7272a745ae0bc53fec521a4767a2001e9f8efb34
Maurizio-P <mauriziopignotti@gmail.com>
2021-10-08 15:51:09 +0200

    complete

---
+++ js/script.js
@@ -0,0 +1,38 @@
+console.log("inizio js")
+
+const costoKm = 0.21;
+let bigliettoUnder18 = ""
+let bigliettoOver65 = ""
+let km = prompt("Inserisci i km da percorrere")
+const costoBiglietto = (km * costoKm).toFixed(2)
+
+console.log("Numero di km: " + km)
+console.log("Costo del biglietto: " + costoBiglietto)
+
+km = parseInt(km)
+
+if (isNaN(km)) {
+    alert("Per favore inserisci un numero valido per i Km.")
+    location.reload()
+}
+
+let eta = prompt("Inserisci la tua età")
+
+console.log("Età del passeggero: " + eta)
+
+eta = parseInt(eta)
+
+if (isNaN(eta)) {
+    alert("Per favore inserisci un numero valido per la tua età.")
+    location.reload()
+} else if (eta <= 17) {
+    bigliettoUnder18 = costoBiglietto - (costoBiglietto / 100 * 20)
+} else if (eta >= 65) {
+    bigliettoOver65 = costoBiglietto - (costoBiglietto / 100 * 40)
+}
+
+console.log(`Prezzo bigglieto under 18: ${bigliettoUnder18}.
+Prezzo biglietto intero: ${costoBiglietto}.
+Prezzo biglietto over 65: ${bigliettoOver65}.`)
+
+
