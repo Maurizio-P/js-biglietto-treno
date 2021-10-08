204dcf6dd89ade3a645b8c8fc746075fb113d63f
Maurizio-P <mauriziopignotti@gmail.com>
2021-10-08 18:04:22 +0200

    add else

--- js/script.js
+++ js/script.js
@@ -14,27 +14,27 @@ km = parseInt(km)
 if (isNaN(km)) {
     alert("Per favore inserisci un numero valido per i Km.")
     location.reload()
-}
-
-let eta = prompt("Inserisci la tua età")
-
-console.log("Età del passeggero: " + eta)
-
-eta = parseInt(eta)
-
-if (isNaN(eta)) {
-    alert("Per favore inserisci un numero valido per la tua età.")
-    location.reload()
-} else if (eta <= 17) {
-    bigliettoUnder18 = (costoBiglietto - (costoBiglietto / 100 * 20)).toFixed(2)
-    prompt("Costo del biglietto under18: " + bigliettoUnder18 + "€")
-} else if (eta >= 65) {
-    bigliettoOver65 = (costoBiglietto - (costoBiglietto / 100 * 40)).toFixed(2)
-    prompt("Costo del biglietto over65: " + bigliettoOver65 + "€")
 } else {
-    prompt("Costo del biglietto intero: " + costoBiglietto + "€")
+    let eta = prompt("Inserisci la tua età")
+    
+    console.log("Età del passeggero: " + eta)
+    
+    eta = parseInt(eta)
+    
+    if (isNaN(eta)) {
+        alert("Per favore inserisci un numero valido per la tua età.")
+        location.reload()
+    } else if (eta <= 17) {
+        bigliettoUnder18 = (costoBiglietto - (costoBiglietto / 100 * 20)).toFixed(2)
+        alert("Costo del biglietto under18: " + bigliettoUnder18 + "€")
+    } else if (eta >= 65) {
+        bigliettoOver65 = (costoBiglietto - (costoBiglietto / 100 * 40)).toFixed(2)
+        alert("Costo del biglietto over65: " + bigliettoOver65 + "€")
+    } else {
+        alert("Costo del biglietto intero: " + costoBiglietto + "€")
+    }
+    
+    console.log(`Prezzo bigglieto under 18: ${bigliettoUnder18}.
+    Prezzo biglietto intero: ${costoBiglietto}.
+    Prezzo biglietto over 65: ${bigliettoOver65}.`)
 }
-
-console.log(`Prezzo bigglieto under 18: ${bigliettoUnder18}.
-Prezzo biglietto intero: ${costoBiglietto}.
-Prezzo biglietto over 65: ${bigliettoOver65}.`)
